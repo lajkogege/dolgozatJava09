51267322db98f95d00f9df7162c020f13b7608a3
lajkogege <lajkogege@gmail.com>
2024-12-09 10:14:15 +0100

    f1 5

--- src/main/java/hu/szamalk/Tábla.java
+++ src/main/java/hu/szamalk/Tábla.java
@@ -1,8 +1,11 @@
 package hu.szamalk;
 
+import java.util.Random;
+
 public class Tábla {
     private  char [][] T = new char[8][8];
     private char ÜresCella;
+    public static Random rnd = new Random();
 
 
     public Tábla() {
@@ -13,10 +16,11 @@ public class Tábla {
         this(üresCella, new char[üresCella][üresCella]);
     }
 
-    public Tábla(char ÜresCella, char [][] T) {
-        this(ÜresCella, char [][] );
+    public Tábla(char ÜresCella, char[] [] T) {
+        this(ÜresCella, T [ÜresCella] [null]);
 
     }
+
 
     public void feltölt(){
         for (int i = 0; i < T.length; i++) {
@@ -29,6 +33,18 @@ public class Tábla {
         System.out.println("4.feladat: Az üres tábla:");
         for (int i = 0; i <T.length ; i++) {
             System.out.print(T[i][i]);
+        }
+    }
+
+    public void Elhelyez(int N){
+        int random= rnd.nextInt(0,1);
+        char jel='K';
+        for (int i = 0; i < N; i++) {
+            if(this.T[i][i] == this.ÜresCella){
+               if (random==1){
+                   this.T[i][i]= [jel] [jel];
+               }
+            }
         }
     }
 
