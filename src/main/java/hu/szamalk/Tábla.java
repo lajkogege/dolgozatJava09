ab6c2380cc7dd9e88a2682cae166d69c9d468290
lajkogege <lajkogege@gmail.com>
2024-12-09 09:21:10 +0100

    f1 2

---
+++ src/main/java/hu/szamalk/Tábla.java
@@ -0,0 +1,14 @@
+package hu.szamalk;
+
+public class Tábla {
+    private char [][] T= new char[8][8];
+    private char ÜresCella;
+
+
+
+    public Tábla(char[][] t, char üresCella) {
+        ÜresCella = üresCella;
+    }
+
+
+}
